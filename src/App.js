b6c2df368c6c396d6c2436e48c9c7899d79b9bcd
bryanmej <bryanmej@gmail.com>
2022-03-17 17:39:54 -0600

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HeaderNav from "./components/HeaderNav";
+import Homepage from "./views/Homepage";
+// import Coinpage from "./views/Coinpage";
+// import Homepage from "./views/Homepage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <HeaderNav />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );

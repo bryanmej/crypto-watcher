eb6a0937b4ed811e1eafbaa64e879f27c0e533d0
unknown <bryanmej@gmail.com>
2022-04-04 22:23:23 -0400

    worked on header

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import "./App.css";
 import HeaderNav from "./components/HeaderNav";
 import Homepage from "./views/Homepage";
 import Coinpage from "./views/Coinpage";
-// import Homepage from "./views/Homepage";
+import {makeStyles} from "@material-ui/core"
 
 function App() {
+
+  const useStyles = makeStyles(()=> ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh",
+    }
+  })) 
+
+  const classes = useStyles()
+
   return (
-    <div className="App">
+    <div className={classes.App}>
       <BrowserRouter>
         <HeaderNav />
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/coin" element={<Coinpage/>} />
+          <Route path="/coin/:id" element={<Coinpage/>} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from "@material-ui/core";
 
 const HeaderNav = () => {
-  return <div>Navigation</div>;
+  return (
+    <AppBar color="transparent" position="static">
+      <Container>
+        <Toolbar>
+          <Typography>Crypto Watcher</Typography>
+          <Select variant="outlined" style={{
+            width: 100,
+            height: 40,
+            marginLeft: 15,
+          }}>
+            <MenuItem>USD</MenuItem>
+            <MenuItem>MXN</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+
+    </AppBar>
+  )
 };
 
 export default HeaderNav;

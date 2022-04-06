f8206f527ead93b53b3217e1b25e4140fe460a82
unknown <bryanmej@gmail.com>
2022-04-05 23:24:30 -0400

    added theme to headernav

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,12 +1,36 @@
 import React from "react";
-import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Container, createTheme, makeStyles, MenuItem, Select, ThemeProvider, Toolbar, Typography } from "@material-ui/core";
+import {useNavigate} from "react-router-dom"
+
+const useStyles = makeStyles(()=>({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontWeight: "bold",
+    cursor: "pointer"
+  }
+}))
+
+
 
 const HeaderNav = () => {
+  const classes = useStyles()
+  const navigate = useNavigate()
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark"
+    }
+  })
+
   return (
+    <ThemeProvider theme={darkTheme}>
     <AppBar color="transparent" position="static">
       <Container>
         <Toolbar>
-          <Typography>Crypto Watcher</Typography>
+          <Typography onClick={()=> navigate("/")} className={classes.title}>Crypto Watcher</Typography>
           <Select variant="outlined" style={{
             width: 100,
             height: 40,
@@ -19,6 +43,7 @@ const HeaderNav = () => {
       </Container>
 
     </AppBar>
+    </ThemeProvider>
   )
 };
 

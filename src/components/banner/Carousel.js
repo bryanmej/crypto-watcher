6c49b17687a8a3e5ef44b86cdf01169cfe790566
unknown <bryanmej@gmail.com>
2022-04-11 22:23:47 -0400

    Carousel ui and logic for displaying coin prices

--- src/components/banner/Carousel.js
+++ src/components/banner/Carousel.js
@@ -1,5 +1,7 @@
 import { makeStyles } from "@material-ui/core"
 import React, {useEffect, useState} from "react"
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
 import { CryptoState } from "../../AppContext";
 import { TrendingCoins } from "../../config/api";
 
@@ -9,8 +11,20 @@ const useStyles = makeStyles((theme) => ({
         height: "50%",
         display: "flex",
         alignItems: "center"
+    },
+    carouselItem: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        cursor: "pointer",
+        textTransform: "uppercase",
+        color: "white"
     }
 }))
+
+const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 const Carousel = () => {
     const [trending, setTrending] = useState([])
@@ -27,13 +41,62 @@ const Carousel = () => {
     useEffect(()=> {
         fetchTrendingCryptos()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currency])
+   }, [currency])
 
-    console.log(trending)
+   const items = trending.map((coin) => {
+       let profit = coin.price_change_percentage_24h >= 0;
+       return (
+           <Link
+            className={classes.carouselItem}
+            to={`/coins/${coin.id}`}
+           >
+               <img 
+                    src={coin.image}
+                    alt={coin.name}
+                    height="80"
+                    style={{ marginBottom: 10}}
+               />
+               <span>
+                   {coin.symbol}
+                   &nbsp;
+                   <span
+                    style={{
+                        color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+                        fontWeight: 500
+                    }}
+                   >
+                        {profit && "+"} {coin.price_change_percentage_24h.toFixed(2)}%
+                   </span>
+                </span>
+                   <span style={{ fontSize: 22, fontWeight: 500}}>
+                        $ {numberWithCommas(coin.current_price.toFixed(2))}
+                   </span>
+               
+           </Link>
+       )
+   })
+
+    const responsive = {
+        0: {
+            items: 2,
+        },
+        512: {
+            items: 4,
+        }
+    }
 
     return (
         <div className={classes.carousel}>
-            carousel
+            <AliceCarousel 
+                mouseTracking
+                infinite
+                autoPlayInterval={1000}
+                animationDuration={1500}
+                disableDotsControls
+                responsive={responsive}
+                autoPlay
+                items={items}
+            />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import AppContext from "./AppContext";
+import 'react-alice-carousel/lib/alice-carousel.css';
+
 
 ReactDOM.render(
     <AppContext>

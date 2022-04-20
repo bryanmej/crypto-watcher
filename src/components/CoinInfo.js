4c6cc4ee1e7759e905796e8030b10044e34ac837
unknown <bryanmej@gmail.com>
2022-04-19 23:47:14 -0400

    added line chart ui

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -8,6 +8,26 @@ import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import { CryptoState } from "../AppContext";
 import { HistoricalChart } from "../config/api";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const CoinInfo = ({ coin }) => {
   const [historyData, setHistoryData] = useState();
@@ -25,8 +45,6 @@ const CoinInfo = ({ coin }) => {
   useEffect(() => {
     fetchHistoryData();
   }, [currency, days]);
-
-  console.log(historyData);
 
   const darkTheme = createTheme({
     palette: {
@@ -67,7 +85,35 @@ const CoinInfo = ({ coin }) => {
             thickness={1}
           />
         ) : (
-          <></>
+          <>
+            <Line
+              data={{
+                labels: historyData.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                      : `${date.getHours()}:${date.getMinutes()} AM`;
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
+
+                datasets: [
+                  {
+                    data: historyData.map((coin) => coin[1]),
+                    label: `Price ( Past ${days} Days ) in ${currency}`,
+                    borderColor: "#EEBC1D",
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  point: {
+                    radius: 1,
+                  },
+                },
+              }}
+            />
+          </>
         )}
       </div>
     </ThemeProvider>
